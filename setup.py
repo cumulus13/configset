43a6464972dad26d06f370b07c184efcf856905e
cumulus13 <cumulus13@gmail.com>
2025-08-18 03:24:51 +0700

    version: 1.60 ~ 2025-08-18 03:24:49:381609

--- setup.py
+++ setup.py
@@ -112,14 +112,14 @@ def get_extras_require():
     return {
         'full': [
             'rich>=10.0.0',
-            'jsoncolor>=0.2.0', 
             'make-colors>=1.0.0',
             'licface'
         ],
         'colors': [
             'rich>=10.0.0',
             'make-colors>=1.0.0',
-            'licface'
+            'licface',
+            'jsoncolor>=0.2.0', 
         ],
         'json': [
             'jsoncolor>=0.2.0'
